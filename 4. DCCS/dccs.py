1cfb8f3724ba923d9e25de976025df9a6be3e69f
Seunghwa <banksemi@naver.com>
2021-06-02 15:14:05 +0900

    DCCS: 튜토리얼 추가

--- 4. DCCS/dccs.py
+++ 4. DCCS/dccs.py
@@ -6,7 +6,7 @@ from PIL import Image
 import random
 import winsound
 import sys             #sys.exit()
-
+import os
 import numpy as np
 import threading
 
@@ -18,100 +18,208 @@ participant_info = inputParticipant()
 win, window = initWindow()
 
 
-basic_left_image = drawling_image(-0.35, +0.2, "Blue_rabbit.png", height=0.35) 
-basic_right_image = drawling_image(+0.35, +0.2, "Red_boat.png", height=0.35) 
+basic_left_image = drawling_image(-0.37, +0.23, "Blue_rabbit.png", height=0.4) 
+basic_right_image = drawling_image(+0.37, +0.23, "Red_boat.png", height=0.4) 
 window.append(basic_left_image)
 window.append(basic_right_image)
 
 basic_plus_image = drawling_text(0, -0.2, "+", color=[0,0,0], height=0.1) 
 window.append(basic_plus_image)
+def trial(right_answer, image_name, timeout=3):
+    winsound.Beep(31111,1)
+    image = images[image_name]
+    trial_result = {}
+    window.update_wait_time(0.5)
+    image.setVisible(True)
+    threading.Thread(target=lambda: winsound.Beep(580,500)).start()
 
-def game(left=[], right=[], count=12):
-    result = {
-        'score': 0,
-        'accuracy': 0,
-        'reaction_time': [],
-        'reaction_time_correct': [],
-        'trials': []
-    }
+    start = datetime.now()
+    while True:
+        window.update()
+        time = (datetime.now()-start).total_seconds()
+            
+        response = 'none'
+        if window.getPressKey('z'):
+            response = 'left'
+        if window.getPressKey('slash'):
+            response = 'right'
+                
+
+        if response != 'none':
+            image.setVisible(False)
+            correct = response == right_answer
+            trial_result['trial_response'] = response
+            trial_result['trial_correct'] = 1 if correct else 0
+            trial_result['trial_time'] = time
+            return trial_result
+
+        if time >= timeout:
+            image.setVisible(False)
+            trial_result['trial_correct'] = 0
+            trial_result['trial_response'] = response
+
+            return trial_result
+
+def game(index, answer, count=12):
+    left=[]
+    right=[]
+
+    for i in answer:
+        if answer[i] == 'left':
+            left.append(i)
+        if answer[i] == 'right':
+            right.append(i)
+    reaction_time= []
+    reaction_time_correct= []
     
     orders = []
-    images = []
     for i in left:
-        image = drawling_image(0, -0.2, i, height=0.35)
-        images.append(image)
-        orders.append(('left', image))
+        orders.append(('left', i))
         
     for i in right:
-        image = drawling_image(0, -0.2, i, height=0.35)
-        images.append(image)
-        orders.append(('right', image))
-        
-    for i in images:
-        i.z = 9
-        i.setVisible(False)
-        window.append(i)
-        
+        orders.append(('right', i))
+
     orders *= int(count / len(orders))
     random.shuffle(orders)
-    for right_answer, image in orders:
-        window.update_wait_time(0.5)
-        image.setVisible(True)
-        threading.Thread(target=lambda: winsound.Beep(580,500)).start()
-
-        start = datetime.now()
-        while True:
-            window.update()
-            time = (datetime.now()-start).total_seconds()
-            
-            response = 'none'
-            if window.getPressKey('z'):
-                response = 'left'
-            if window.getPressKey('slash'):
-                response = 'right'
-                
-            if response != 'none':
-                image.setVisible(False)
-                correct = response == right_answer
-                result['trials'].append([response, 1 if correct else 0, time])
-                
-                if correct:
-                    result['score'] += 1
-                    result['reaction_time_correct'].append(time)
-                
-                result['reaction_time'].append(time)
-                break
     
-    result['reaction_time_correct'] = np.array(result['reaction_time_correct']).mean()
-    result['reaction_time'] = np.array(result['reaction_time']).mean()
-    result['accuracy'] = result['score'] / count
-    return result
-    
-def save(index, xlsx, game_result):
     prefix = 'trial%d_' % index
-    result.write(prefix + 'score', game_result['score'])
-    result.write(prefix + 'reaction_time', game_result['reaction_time'])
-    result.write(prefix + 'reaction_time_correct', game_result['reaction_time_correct'])
-    
-    for i, row in enumerate(game_result['trials']):
-        result.write(prefix + 'each_response', row[0], index=i)
-        result.write(prefix + 'each_correct', row[1], index=i)
-        result.write(prefix + 'each_time', row[2], index=i)
+    for trial_index, (right_answer, image_name) in enumerate(orders):
+        trial_result = trial(right_answer, image_name, timeout=15) # 본 시행시 무반응 시간
+
+        if trial_result['trial_correct']:
+            reaction_time_correct.append(trial_result['trial_time'])
+        reaction_time.append(trial_result['trial_time'])
+
+        for i in trial_result:
+            result.write(i.replace('trial_', prefix + 'each_'), trial_result[i], index=trial_index)
+        result.save()
+        
+    result.write(prefix + 'reaction_time', np.array(reaction_time).mean())
+    result.write(prefix + 'reaction_time_correct', np.array(reaction_time_correct).mean())
     result.save()
+    
+
 # 기본값 Blue_rabbit, Red_boat
 result = pyresult(participant_info, 'DCCS')
 
-# 색깔로 맞추기
-showExplanation(["T1.png", "T2.png", "T3.png", "T4.png", "T5.png", "T6.png", "T7.png", "T8.png"])
-save(1, result, game(["Blue_boat.png"], ["Red_rabbit.png"]))
+# 사용할 이미지를 사전 등록
+images = {}
+for i in os.listdir("./이미지"):
+    image_path = os.path.join("./이미지", i)
+    image = drawling_image(0, -0.23, image_path, height=0.4)
+    image.z = 9
+    image.setVisible(False)
+    window.append(image)
+    images[i] = image
 
-# 모양으로 맞추기
-showExplanation(["TT1.png", "TT2.png", "TT3.png", "TT4.png", "TT5.png", "TT6.png", "TT7.png", "TT8.png"])
-save(2, result, game(["Red_rabbit.png"], ["Blue_boat.png"]))
+explaning = ['./튜토리얼/DCCS(A)_T%d.PNG' % i for i in range(0,60)]
+# 색깔로 맞추기
+
+practice = lambda x: trial(answer[x], x)['trial_correct'] == 1
+
+'''
+showExplanation(explaning[1:1+9])
+answer = {
+    "blue_boat.png": 'left',
+    "red_rabbit.png": 'right'
+}
+
+# 연습시행 1
+if not practice("red_rabbit.png"):
+    showExplanation(explaning[11:11+3])
+ 
+if practice("blue_boat.png"):
+    showExplanation(explaning[10])
+else:
+    showExplanation(explaning[14:14+2])
+    showExplanation(explaning[13])
+# 연습시행 2
+while True:
+    if not practice("blue_boat.png"):
+        showExplanation(explaning[14:14+2])
+        showExplanation(explaning[13])
+
+    if practice("red_rabbit.png"):
+        showExplanation(explaning[16])
+        break
+    else:
+        showExplanation(explaning[11:11+3])
+
+game(1, answer)
+
+answer = {
+    "blue_boat.png": 'right',
+    "red_rabbit.png": 'left'
+}
+
+
+if not practice("red_rabbit.png"):
+    showExplanation(explaning[25:25+3])
+ 
+if practice("blue_boat.png"):
+    showExplanation(explaning[24])
+else:
+    showExplanation(explaning[28:28+2])
+    showExplanation(explaning[27])
+# 연습시행 2
+while True:
+    if not practice("blue_boat.png"):
+        showExplanation(explaning[28:28+2])
+        showExplanation(explaning[27])
+
+    if practice("red_rabbit.png"):
+        showExplanation(explaning[30])
+        break
+    else:
+        showExplanation(explaning[25:25+3])
+game(2, answer)
+
 
 # 테두리 -> 색깔,  일반 -> 모양
-showExplanation(["TTT1.png", "TTT2.png", "TTT3.png", "TTT4.png", "TTT5.png", "TTT6.png", "TTT7.png",  "TTT15.png", "TTT16.png", "TTT8.png", "TTT9.png", "TTT10.png", "TTT11.png", "TTT12.png", "TTT13.png", "TTT14.png"])
-save(3, result, game(["blue_boat_square.png", "Red_rabbit.png"], ["red_rabbit_square.png", "Blue_boat.png"]))
+answer = {
+    "blue_boat_square.png": 'left',
+    "red_rabbit_square.png": 'right',
+    "blue_boat.png": 'right',
+    "red_rabbit.png": 'left',
+}
+
+if not practice("blue_boat_square.png"):
+    showExplanation(explaning[46:46+3])
+
+if not practice("red_rabbit_square.png"):
+    showExplanation(explaning[49:49+2])
+    showExplanation(explaning[48])
+
+if not practice("blue_boat.png"):
+    showExplanation(explaning[51:51+2])
+    showExplanation(explaning[48])
+
+if practice("red_rabbit.png"):
+    showExplanation(explaning[45])
+else:
+    showExplanation(explaning[53:53+2])
+    showExplanation(explaning[48])
+
+
+while True:
+    if not practice("red_rabbit_square.png"):
+        showExplanation(explaning[49:49+2])
+        showExplanation(explaning[48])
+
+    if not practice("blue_boat.png"):
+        showExplanation(explaning[51:51+2])
+        showExplanation(explaning[48])
+
+    if not practice("red_rabbit.png"):
+        showExplanation(explaning[53:53+2])
+        showExplanation(explaning[48])
+
+    if practice("blue_boat_square.png"):
+        showExplanation(explaning[55])
+        break
+    else:
+        showExplanation(explaning[46:46+3])
+game(3, answer)
 
 
 result.save()
