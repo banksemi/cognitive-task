e33cc95449affb0d645bca6201346336ce3d8d0a
Seunghwa <banksemi@naver.com>
2021-06-18 16:13:57 +0900

    설치 batch 파일 수정

--- 설치 방법/2. 파이썬 필수 패키지 설치.bat
+++ 설치 방법/2. 파이썬 필수 패키지 설치.bat
@@ -1,6 +1,8 @@
+@Echo off
 call C:\Users\%USERNAME%\anaconda3\Scripts\activate.bat
 pip install psychopy
 pip install playsound
 pip install mouse
 pip install pipwin 
 pipwin install pyaudio
+pause
